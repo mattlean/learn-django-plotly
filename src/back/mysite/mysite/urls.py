023cd35ea2aff6070618874798c05ddac0d29158
Matthew Lean <matt@mattlean.com>
2020-11-10 22:43:04 -0800

    Finish Django REST framework quickstart

--- src/back/mysite/mysite/urls.py
+++ src/back/mysite/mysite/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from quickstart import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('polls/', include('polls.urls'))
 ]
