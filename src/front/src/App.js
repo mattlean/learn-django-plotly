5ba018f5bedc9f23b8e27fbb639ea6fafff9e022
Matthew Lean <matt@mattlean.com>
2020-11-10 23:56:54 -0800

    Experiment with region detection for mouse click and hover events

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Plot from 'react-plotly.js';
 
+const regionA = [
+  {x: '2015-02-04', y: 0},
+  {x: '2015-02-08', y: 1}
+]
+const regionB = [
+  {x: '2015-02-20', y: 0},
+  {x: '2015-02-24', y: 1}
+]
+
 function App() {
+  const mousePointerRef = useRef()
+
+  useEffect(() => {
+    mousePointerRef.current = document.getElementsByClassName('nsewdrag')[0]
+  }, [])
+
+  function clickHandler(data) {
+    const { x: currX } = data.points[0]
+
+    if(currX >= regionA[0].x && currX <= regionA[1].x) {
+      console.log('Region A selected')
+    } else {
+      console.log('Region A not selected')
+    }
+
+    if(currX >= regionB[0].x && currX <= regionB[1].x) {
+      console.log('Region B selected')
+    } else {
+      console.log('Region B not selected')
+    }
+
+    console.log('---')
+  }
+
+  function hoverHandler(data) {
+    const { x: currX } = data.points[0]
+
+    if(
+      currX >= regionA[0].x && currX <= regionA[1].x ||
+      currX >= regionB[0].x && currX <= regionB[1].x
+    ) {
+      mousePointerRef.current.style.cursor = 'pointer'
+    } else {
+      mousePointerRef.current.style.cursor = ''
+    }
+  }
+
   return (
     <div className="App">
       <Plot
@@ -25,10 +71,10 @@ function App() {
                   type: 'rect',
                   xref: 'x',
                   yref: 'paper',
-                  x0: '2015-02-04',
-                  y0: 0,
-                  x1: '2015-02-08',
-                  y1: 1,
+                  x0: regionA[0].x,
+                  y0: regionA[0].y,
+                  x1: regionA[1].x,
+                  y1: regionA[1].y,
                   fillcolor: '#28A745',
                   opacity: 0.2,
                   line: {
@@ -39,10 +85,10 @@ function App() {
                   type: 'rect',
                   xref: 'x',
                   yref: 'paper',
-                  x0: '2015-02-20',
-                  y0: 0,
-                  x1: '2015-02-24',
-                  y1: 1,
+                  x0: regionB[0].x,
+                  y0: regionB[0].y,
+                  x1: regionB[1].x,
+                  y1: regionB[1].y,
                   fillcolor: '#ffc107',
                   opacity: 0.2,
                   line: {
@@ -53,6 +99,8 @@ function App() {
           height: 500,
           width: 500
         } }
+        onClick={clickHandler}
+        onHover={hoverHandler}
       />
     </div>
   );
